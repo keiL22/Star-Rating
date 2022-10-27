a2b925577b016cd52cddd1597ff22390c2cb2565
Keisuke <keisuke.lester@my.utsa.edu>
2022-10-26 20:00:07 -0500

    added logic+annimation delay

--- JavaScript/StarLogic.js
+++ JavaScript/StarLogic.js
@@ -13,23 +13,51 @@ rating.addEventListener("keydown", function (input) {
 
 function validate(input) {
     var text = input.target.value;
+    const result = parseFloat(text);
+    input.target.value = '';
     if(isNaN(text)){
         alert(text + " is not a number");
-        input.target.value = '';
-    }
-    else{
-        const result = parseFloat(text);
-        input.target.value = '';
+    }else if (result>100){
+        alert(text + " is too large (Enter 100 or Less)");
+    }else{
         percentage(result);
     }
 }
 
 function percentage(result){
-    if(result === 40){
+    if(result < 20){
         starOne.classList.add("star-on");
-        starOne.classList.remove("star-off");
-    }else{
-        starOne.classList.add("star-off");
-        starOne.classList.remove("star-on");
+
+        starTwo.classList.remove("star-on");
+        starThree.classList.remove("star-on");
+        starFour.classList.remove("star-on");
+        starFive.classList.remove("star-on");
+    }else if(result >= 20 && result < 40){
+        starTwo.classList.add("star-on");
+        starOne.classList.add("star-on");
+
+        starThree.classList.remove("star-on");
+        starFour.classList.remove("star-on");
+        starFive.classList.remove("star-on");
+    }else if(result >= 40 && result < 60){
+        starTwo.classList.add("star-on");
+        starOne.classList.add("star-on");
+        starThree.classList.add("star-on");
+
+        starFour.classList.remove("star-on");
+        starFive.classList.remove("star-on");
+    }else if(result >= 60 && result < 80){
+        starTwo.classList.add("star-on");
+        starOne.classList.add("star-on");
+        starThree.classList.add("star-on");
+        starFour.classList.add("star-on");
+
+        starFive.classList.remove("star-on");
+    }else if(result <= 100){
+        starTwo.classList.add("star-on");
+        starOne.classList.add("star-on");
+        starThree.classList.add("star-on");
+        starFour.classList.add("star-on");
+        starFive.classList.add("star-on");
     }
 }
